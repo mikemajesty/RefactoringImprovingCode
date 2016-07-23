64914a37ebdde10c7c063340a56cd90c7037899a
mikemajesty <mikemajesty2011@gmail.com>
2016-07-23 12:37:41 -0300

    Chapter 1 - Extracting Frequent Renter Points

--- rental/Customer.java
+++ rental/Customer.java
@@ -14,33 +14,25 @@ class Customer {
     return _name;
   };
 
-    public String statement() {
-      double totalAmount = 0;
-      int frequentRenterPoints = 0;
-      Enumeration rentals = _rentals.elements();
-      String result = "Rental Record for " + getName() + "\n";
-      while (rentals.hasMoreElements()) {
-        Rental each = (Rental) rentals.nextElement();
-        // add frequent renter points
-        frequentRenterPoints ++;
-        // add bonus for a two day new release rental
-        if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE)
-        &&
-        each.getDaysRented() > 1) frequentRenterPoints ++;
-        //show figures for this rental
-        result += "\t" + each.getMovie().getTitle()+ "\t" +
-        String.valueOf
-        (each.getCharge()) + "\n";
-        totalAmount += each.getCharge();
-      }
-      //add footer lines
-      result += "Amount owed is " + String.valueOf(totalAmount) +
-      "\n";
-      result += "You earned " + String.valueOf(frequentRenterPoints)
-      + " frequent renter points";
-      return result;
-      }
+  public String statement() {
+    double totalAmount = 0;
+    int frequentRenterPoints = 0;
+    Enumeration rentals = _rentals.elements();
+    String result = "Rental Record for " + getName() + "\n";
+    while (rentals.hasMoreElements()) {
+      Rental each = (Rental) rentals.nextElement();
+      frequentRenterPoints += each.getFrequentRenterPoints();
+      //show figures for this rental
+      result += "\t" + each.getMovie().getTitle()+ "\t" +
+      String.valueOf(each.getCharge()) + "\n";
+      totalAmount += each.getCharge();
     }
+    //add footer lines
+    result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
+    result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
+    return result;
+
+  }
 
   private double amountFor(Rental aRental) {
     double result = 0;
