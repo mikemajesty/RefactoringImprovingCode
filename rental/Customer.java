c15471c7049f8d7ef2ce469f5954015d38a3a142
mikemajesty <mikemajesty2011@gmail.com>
2016-07-28 22:10:19 -0300

    Chapter 1 - Removing Temps

--- rental/Customer.java
+++ rental/Customer.java
@@ -15,23 +15,28 @@ class Customer {
   };
 
   public String statement() {
-    double totalAmount = 0;
-    int frequentRenterPoints = 0;
     Enumeration rentals = _rentals.elements();
     String result = "Rental Record for " + getName() + "\n";
     while (rentals.hasMoreElements()) {
       Rental each = (Rental) rentals.nextElement();
-      frequentRenterPoints += each.getFrequentRenterPoints();
       //show figures for this rental
       result += "\t" + each.getMovie().getTitle()+ "\t" +
       String.valueOf(each.getCharge()) + "\n";
-      totalAmount += each.getCharge();
     }
     //add footer lines
-    result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
-    result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
+    result += "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
+    result += "You earned " +  String.valueOf(getTotalFrequentRenterPoints()) + " frequent renter points";
     return result;
+  }
 
+  private int getTotalFrequentRenterPoints(){
+    int result = 0;
+    Enumeration rentals = _rentals.elements();
+    while (rentals.hasMoreElements()) {
+      Rental each = (Rental) rentals.nextElement();
+      result += each.getFrequentRenterPoints();
+    }
+    return result;
   }
 
   private double amountFor(Rental aRental) {
